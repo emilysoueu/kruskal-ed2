789f6d37056b29353a3ce5694ed14c63b71e95a1
Unknown <emilys2costa@gmail.com>
2019-04-20 02:17:17 -0300

    half work

--- Algoritmos de Ordenação/quickSort.py
+++ Algoritmos de Ordenação/quickSort.py
@@ -1,3 +1,20 @@
+# Escolher o pivo
+#Separe os elementos menores que o pivo para a esquerda e os elementos maiotrs que o pivo para a direita
+# Recursivamente execute o mesmo procedimento para os elementos a esquerda do pivo
+# recursivamente execute o mesmo procedimento para os elementos a direita do pivo
+
+
+#pivo: primeiro
+#pivo: último
+#pivo: mediana entre primeiro, meio e fim
+
+
+#Separar o vetor
+	#Definir [i] e [j] começando pela esquerda e direita, respectivamente
+	#faça uma varreddura com a variável [i] para a direita até que encontre um elementov[i] maior que o pivo
+	#faça uma varredira com a variavél [j] para a esquerda até que encontre um elemento v[j] menor que o pivô
+	#troque v[i] com v[j]
+
 import random
 
 def quicksort(arr, start, end, pivot_mode='random'):
@@ -8,16 +25,60 @@ def quicksort(arr, start, end, pivot_mode='random'):
 	return arr
 
 def partition(arr, start, end, pivot_mode):
-	if pivot_mode == 'first':
+	if pivot_mode == 'first': #Pivo Inicio
 		pivot = arr[start]
-	else:
+	elif pivot_mode == 'end':#Pivo fim
+		pivot = arr[end]
+	elif pivot_mode == 'median' #Pivo mediana entre inicio, central e fim
+		half = int((start+end)/2)
+		s = A[star]
+		half = A[half]
+		e = A[end]
+	else: #pivo aleatório
 		pivot_index = random.randrange(start,end)
 		pivot = arr[pivot_index]
 		arr[pivot_index], arr[start] = arr[start], arr[pivot_index] # place the pivot at the beginning
 	i = start + 1
+
 	for j in range(start+1,end+1):
 		if arr[j] < pivot:
 			arr[i], arr[j] = arr[j], arr[i]
 			i += 1
 	arr[start], arr[i-1] = arr[i-1], arr[start]
 	return i-1
+
+#------------------------------------------------------------
+'''
+def quick_sort(A, L, R):
+	if (L < R):
+		j = separar(A,L,R)
+		quicksort(A,L,j-1)
+		quicksort(A,j+1,R)
+
+def separar(A,L,R):
+	i = L+1
+	j = R
+	pivo = A[L]
+
+	while(i<=j):
+		if A[i] <= pivo #acha em [i] a primeira posição maior que o pivo
+			i++
+		elif A[j] >pivo: #acha em [j] a primeira posição menor que o pivo		
+			j--
+		elif i<=j : #efetuar a troca
+			trocar(A,i,j)
+			i++
+			j--
+
+	trocar(A,L,j)
+	return j
+
+def trocar(V,i,j):
+	aux = V[i]
+	V[i]= V[j]
+	V[j]= aux
+'''
+#--------------------------------------------------------------
+
+
+
--- trabalho/algoritmosDeOrdenação.py
+++ trabalho/algoritmosDeOrdenação.py
@@ -1,174 +1,133 @@
-#@EmilyCosta
-#16/04/2017
-
 '''
 Introdução:
 - Implementar algoritmo de ordenação que receba uma colecão
 - A coleção é uma lista de arestas
 - Para comparar o peso as arestas entre dois item da coleção basta usar a chave 'weight' (peso)
-
 Exemplos:
 - Modo convencional
 colecao[i] operador de comparacao colecao[j]
 colecao[i] < colecao[j]
-
 - Modo que você vai usar
 int(colecao[i]['weight']) operador de comparacao int(colecao[j]['weight'])
 int(colecao[i]['weight']) < int(colecao[j]['weight'])
-
 É nescessário converter o valor pra Interger no momento da comparação a fim de evitar erros
 '''
 
-
 # Sua classe algoritmo de ordenação precisar ter um método ordenar
-#========================================================================================================#
-#
-#
-#========================================================================================================#
-class InsertionSort(object):
-    def ordenar(self, colecao):     
-		for i in range(1, len(colecao)):			
-			for j in range (i-1,0,-1):
-				if (colecao[j]['weight'])> (colecao[i]['weight']): #alteração aqui para o mode de apresentação
-					(colecao[j+1]) = (colecao[j])
-				else:
-					(colecao[j+1]) = (colecao[i])
-					break
 
-        return colecao
-#========================================================================================================#
-#
-#
-#========================================================================================================#
-def selection_sort(A):
-	 for i in range (0, len(A) - 1) :
-	 	minIndex = i
-	 	for j in range (i+1, len(A)) :
-	 		if A[j] < A[minIndex]:
-	 			minIndex = j
-	 		if minIndex != i:
-	 			A[i],A[minIndex] = A[minIndex], A[i]
-#========================================================================================================#
-#
-#
-#========================================================================================================#
-def shellSort(arr, n):
- 	h = 1; #our steps for sorting
- 	while (h < n/2):
- 		h = 2*h+1; #calculate max step needed
- 	while (h>=1):
- 		for i in range (h,n):
- 			temp = arr[i];
- 			j = i;
- 			while (j >= h and arr[j] < arr[j-h]):
- 				arr[j] = arr[j-h];
- 				j = j-h;
- 			arr[j] = temp;
- 		h/= 2;
 
-myL = [3, 1 , 6, 2, 8, 4];
-
-shellSort(myL, 6);
-print (myL);
-#========================================================================================================#
-#
-#
-#========================================================================================================#
 import random
 
-def quicksort(arr, start, end, pivot_mode='random'):
-	if start < end:
-		split = partition(arr, start, end, pivot_mode)
-		quicksort(arr, start, split-1, pivot_mode)
-		quicksort(arr, split+1, end, pivot_mode)
-	return arr
+#======================================================================================#
+#   							I N S E R T I O N
+#======================================================================================#
 
-def partition(arr, start, end, pivot_mode):
-	if pivot_mode == 'first':
-		pivot = arr[start]
-	else:
-		pivot_index = random.randrange(start,end)
-		pivot = arr[pivot_index]
-		arr[pivot_index], arr[start] = arr[start], arr[pivot_index] # place the pivot at the beginning
-	i = start + 1
-	for j in range(start+1,end+1):
-		if arr[j] < pivot:
-			arr[i], arr[j] = arr[j], arr[i]
-			i += 1
-	arr[start], arr[i-1] = arr[i-1], arr[start]
-	return i-1
+class insertion_sort(object):
+	def ordenar(self, colecao):
+		for i in range(1, len(colecao)): # [i] percorrer da pos 1 até a última pos de colecao
+			'''print('Vetor ',i,': %s' %colecao) #teste 0'''
+			curNum = colecao[i] #coleção
+			'''print('curNum = ',curNum) #teste 1'''
+			for j in range (i-1,-1,-1):# [j] percorre da primeira pos
+				'''print('[J] = ', j)'''
+				if colecao[j]['weight']>curNum['weight']: # procura para frente até encontrar alguém menor que o atual num
+					temp = colecao[j+1]
+					colecao[j+1] =colecao[j]				
+					colecao[j] = temp
+					'''
+					print(colecao[j],' >= ',curNum, '= true')  #teste 2
+					print('temp = ',temp)
+					'''
+				else:
+					colecao[j+1] = curNum
+					break
+		return colecao
+#======================================================================================#
+#   					 		S E L E C T I O N
+#======================================================================================#
 
-#========================================================================================================#
-#
-#
-#========================================================================================================#
-def merge_sort(A):
-	merge_sort2(A, 0, len(A)-1)
 
-def merge_sort2(A, first, last):
-	if first < last:
-		middle = (first+last)//2
-		merge_sort2(A, first, middle)
-		merge_sort2(A, middle+1, last)
-		merge(A, first, middle,last)
+class selection_sort(object):
+	def ordenar(self,colecao):
+		for i in range (0, len(colecao) - 1) :
+		 	minIndex = i
+		 	for j in range (i+1, len(colecao)):
+		 		if colecao[j]['weight'] < colecao[minIndex]['weight']:
+		 			minIndex = j
+		 		if minIndex != i:
+		 			colecao[i],colecao[minIndex] = colecao[minIndex], colecao[i]
+		return colecao
+#======================================================================================#
+#   							 S H E L L S O R T
+#======================================================================================#
 
-def merge(A, first, middle, last):
-	L = A[first:middle]
-	R = A[middle:last+1]
-	L.append(999999999)
-	R.append(999999999)
-	i = j = 0
 
-	for k in range (first, last+1):
-		if L[i] <= R[j]:
-			A[k] =L[i]
-			i+= 1
+class shell_sort(object):
+	def ordenar(self,colecao):
+		h=1
+		for h in range (1, int(len(colecao)), 3*h+1):
+			while (h>=1):
+				h = int((h-1)/3) #fazer cast não dar prob de float
+				i=h
+				while (i<len(colecao)):
+					aux = colecao[i]
+					j=i
+					while (j >= h and int(aux['weight']) < int(colecao[j-h]['weight'])):
+						colecao[j] = colecao[j-h]
+						j = j-h
+					colecao[j] = aux
+					i = i+1
+		h = h/2
+		return colecao
+
+#======================================================================================#
+#   							H E A P S O R T
+#======================================================================================#
+
+class heapsort(object):
+
+	def ordenar(self,colecao):
+		def build_max_heap(colecao):
+
+			for i in range(len(colecao) - 1, 0, -1):
+				colecao[0], colecao[i] = colecao[i], colecao[0]
+				self.max_heapify(colecao, index=0, size=i)
+
+			return colecao
+	
+	def parent(i):
+		return (i - 1)//2
+	
+	def left(i):
+		return 2*i + 1
+	
+	def right(i):
+		return 2*i + 2
+	
+	def build_max_heap(colecao):
+		length = len(colecao)
+		start = parent(length - 1)
+		while start >= 0:
+			max_heapify(colecao, index=start, size=length)
+			start = start - 1
+	
+	def max_heapify(colecao, index, size):
+		l = left(index)
+		r = right(index)
+		if (l < size and colecao[l]['weight'] > colecao[index]['weight']):
+			largest = l
 		else:
-			A[k] = R[j]
-			j += 1
+			largest = index
+		if (r < size and colecao[r]['weight'] > colecao[largest]['weight']):
+			largest = r
+		if (largest != index):
+			colecao[largest], colecao[index] = colecao[index], colecao[largest]
+			max_heapify(colecao, largest, size)
 
-#========================================================================================================#
-#
-#
-#========================================================================================================#
-def heapsort(alist):
-    build_max_heap(alist)
-    for i in range(len(alist) - 1, 0, -1):
-        alist[0], alist[i] = alist[i], alist[0]
-        max_heapify(alist, index=0, size=i)
- 
-def parent(i):
-    return (i - 1)//2
- 
-def left(i):
-    return 2*i + 1
- 
-def right(i):
-    return 2*i + 2
- 
-def build_max_heap(alist):
-    length = len(alist)
-    start = parent(length - 1)
-    while start >= 0:
-        max_heapify(alist, index=start, size=length)
-        start = start - 1
- 
-def max_heapify(alist, index, size):
-    l = left(index)
-    r = right(index)
-    if (l < size and alist[l] > alist[index]):
-        largest = l
-    else:
-        largest = index
-    if (r < size and alist[r] > alist[largest]):
-        largest = r
-    if (largest != index):
-        alist[largest], alist[index] = alist[index], alist[largest]
-        max_heapify(alist, largest, size)
-#========================================================================================================#
-#
-#
-#========================================================================================================#
+#======================================================================================#
+#   							C O U N T S O R T
+#======================================================================================#
+
 
 def get_sortkey(n):
     """ Define the method to retrieve the key """
@@ -198,7 +157,61 @@ def counting_sort(tlist, k, get_sortkey):
         count_list[sortkey] -=1
  
     return output
- 
-			 
 
 
+#======================================================================================#
+#   							M E R G E S O R T
+#======================================================================================#
+class merge_sort(object):
+	def ordenar(self, colecao):
+		merge_sort2(colecao, 0, len(colecao)-1)
+
+	def merge_sort2(colecao, first, last):
+		if first < last:
+			middle = (first+last)//2
+			merge_sort2(colecao, first, middle)
+			merge_sort2(colecao, middle+1, last)
+			merge(colecao, first, middle,last)
+
+	def merge(colecao, first, middle, last):
+		L = colecao[first:middle]
+		R = colecao[middle:last+1]
+		L.append(999999999)
+		R.append(999999999)
+		i = j = 0
+
+		for k in range (first, last+1):
+			if L[i] <= R[j]:
+				colecao[k] =L[i]
+				i+= 1
+			else:
+				colecao[k] = R[j]
+				j += 1
+
+
+#======================================================================================#
+#   							Q U I C K S O R T
+#======================================================================================#
+
+class  quicksort(object):
+	def ordenar(self,arr, start, end, pivot_mode='random'):
+		if start < end:
+			split = self.partition(arr, start, end, pivot_mode)
+			quicksort(arr, start, split-1, pivot_mode)
+			quicksort(arr, split+1, end, pivot_mode)
+		return arr
+
+	def partition(arr, start, end, pivot_mode):
+		if pivot_mode == 'first':
+			pivot = arr[start]
+		else:
+			pivot_index = random.randrange(start,end)
+			pivot = arr[pivot_index]
+			arr[pivot_index], arr[start] = arr[start], arr[pivot_index] # place the pivot at the beginning
+		i = start + 1
+		for j in range(start+1,end+1):
+			if arr[j] < pivot:
+				arr[i], arr[j] = arr[j], arr[i]
+				i += 1
+		arr[start], arr[i-1] = arr[i-1], arr[start]
+		return i-1
